bc06996b4ba9790f12349b690fffd4f12f37a7a3
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-06 18:35:48 +0700

    Initial commit

---
+++ src/main/java/com/se330/uitbooknetwork/UitBookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.se330.uitbooknetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitBookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitBookNetworkApplication.class, args);
+	}
+
+}
