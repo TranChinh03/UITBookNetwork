7b207935654ac4235e0941805bac759204df39a4
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-09 00:38:57 +0700

    fea: save book

---
+++ src/main/java/com/se330/uitbooknetwork/book/BookRepository.java
@@ -0,0 +1,7 @@
+package com.se330.uitbooknetwork.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
