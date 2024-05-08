732e20208127e697024b28335a12bfff1354a607
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-08 23:41:57 +0700

    Create entities + relationships

--- src/main/java/com/se330/uitbooknetwork/user/User.java
+++ src/main/java/com/se330/uitbooknetwork/user/User.java
@@ -1,5 +1,7 @@
 package com.se330.uitbooknetwork.user;
 
+import com.se330.uitbooknetwork.book.Book;
+import com.se330.uitbooknetwork.history.BookTransactionHistory;
 import com.se330.uitbooknetwork.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -44,6 +46,12 @@ public class User implements UserDetails, Principal {
     // Private list of roles List<Role>;
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
