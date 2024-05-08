92c4630c3d7e7f72358a19cbb14d62536b123475
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-09 02:04:00 +0700

    fea: find books + update book status

--- src/main/java/com/se330/uitbooknetwork/book/BookMapper.java
+++ src/main/java/com/se330/uitbooknetwork/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.se330.uitbooknetwork.book;
 
+import com.se330.uitbooknetwork.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,33 @@ public class BookMapper {
                 .synopsis(request.synopsis())
                 .archived(false)
                 .shareable(request.shareable())
+                .build();
+    }
+
+    public BookResponse toBookResponse(Book book) {
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .authorName(book.getAuthorName())
+                .isbn(book.getIsbn())
+                .synopsis(book.getSynopsis())
+                .rate(book.getRate())
+                .archived(book.isArchived())
+                .shareable(book.isShareable())
+                .owner(book.getOwner().getFullName())
+                //.cover
+                .build();
+    };
+
+    public BorrowedBookResponse toBorrowedBookResponse(BookTransactionHistory history) {
+        return BorrowedBookResponse.builder()
+                .id(history.getBook().getId())
+                .title(history.getBook().getTitle())
+                .authorName(history.getBook().getAuthorName())
+                .isbn(history.getBook().getIsbn())
+                .rate(history.getBook().getRate())
+                .returned(history.isReturned())
+                .returnApproved(history.isReturnApproved())
                 .build();
     }
 }
