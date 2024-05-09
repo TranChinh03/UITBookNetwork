bbde050d02803d4dd7857898ff1b76084396998f
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-09 16:16:55 +0700

    fea: save + find feedback

---
+++ src/main/java/com/se330/uitbooknetwork/feedback/FeedbackResponse.java
@@ -0,0 +1,14 @@
+package com.se330.uitbooknetwork.feedback;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedbackResponse {
+    private Double note;
+    private String comment;
+    private boolean ownFeedback;
+}
