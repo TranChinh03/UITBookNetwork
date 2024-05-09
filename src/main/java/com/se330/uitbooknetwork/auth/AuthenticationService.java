a249da620e3000506dad57aa9fe485b8cd0ba8a4
TranChinh03 <21520166@gm.uit.edu.vn>
2024-05-09 15:21:25 +0700

    fea: upload book cover

--- src/main/java/com/se330/uitbooknetwork/auth/AuthenticationService.java
+++ src/main/java/com/se330/uitbooknetwork/auth/AuthenticationService.java
@@ -37,7 +37,7 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
 
-    @Value("${application.security.mailing.frontend.activation-url}")
+    @Value("${application.mailing.frontend.activation-url}")
     private String activationUrl;
 
     public void register(RegistrationRequest request) throws MessagingException {
--- src/main/java/com/se330/uitbooknetwork/book/BookService.java
+++ src/main/java/com/se330/uitbooknetwork/book/BookService.java
@@ -2,6 +2,7 @@ package com.se330.uitbooknetwork.book;
 
 import com.se330.uitbooknetwork.common.PageResponse;
 import com.se330.uitbooknetwork.exception.OperationNotPermittedException;
+import com.se330.uitbooknetwork.file.FileStorageService;
 import com.se330.uitbooknetwork.history.BookTransactionHistory;
 import com.se330.uitbooknetwork.history.BookTransactionHistoryRepository;
 import com.se330.uitbooknetwork.user.User;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Objects;
@@ -26,6 +28,9 @@ public class BookService {
     private final BookRepository bookRepository;
     private final BookTransactionHistoryRepository transactionHistoryRepository;
     private final BookMapper bookMapper;
+
+    private final FileStorageService fileStorageService;
+
     public Integer save(BookRequest request, Authentication connectedUser) {
         User user = (User) connectedUser.getPrincipal();
         Book book = bookMapper.toBook(request);
@@ -196,5 +201,14 @@ public class BookService {
                 .orElseThrow(() -> new OperationNotPermittedException("The book is not returned."));
         bookTransactionHistory.setReturnApproved(true);
         return transactionHistoryRepository.save(bookTransactionHistory).getId();
+    }
+
+    public void uploadBookCoverPicture(MultipartFile file, Authentication connectedUser, Integer bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID: " + bookId));
+        User user = (User) connectedUser.getPrincipal();
+        var bookCover = fileStorageService.saveFile(file, user.getId());
+        book.setBookCover(bookCover);
+        bookRepository.save(book);
     }
 }
